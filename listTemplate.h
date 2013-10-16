6b307b825721c6f7e930b77422ec9f718f8d3613
Terence Schumacher <terenceschumacher@gmail.com>
2013-10-16 11:01:01 -0700

    Create listTemplate.h
    created the .h template file that we will be submitting

---
+++ listTemplate.h
@@ -0,0 +1,31 @@
+
+/** 
+    @file listTemplate.h
+    @brief Full ordered List class turned into a template
+           of objects
+    @author Lance Cho
+    @author Terence Schumacher
+    @version 1.0
+    
+*/
+
+
+
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+//This file should contain all class def and function code
+
+
+
+
+
+
+#endif
